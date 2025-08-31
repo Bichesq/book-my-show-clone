334dcd5cfd755f5bdf162555a99562d34338eb52
Deodatus Biche <bichesq@gmail.com>
2025-08-31 17:25:54 +0530

    oompleted the hero carousel next/prev button issue

--- src/components/heroCarousel/arrow.tsx
+++ src/components/heroCarousel/arrow.tsx
@@ -25,10 +25,10 @@ export const NextArrow = (props: any) => {
 
 export const PrevArrow = (props: any) => {
     return (
-        <div>
-            <div className={props.className} style={{ ...props.style }} onClick={props.onClick} >
-                <GrPrevious />
-            </div>
+      
+        <div className={props.className + "mx-1 p-2.5 top-1/2 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white border-2 border-gray-900 align-center justify-center"} style={{ ...props.style, position: "absolute", left: "10px", display: "block", zIndex: 10, top: "50%", transform: "translateY(-50%)" }} onClick={props.onClick} >
+            <GrPrevious />
         </div>
+        
     );
 }
